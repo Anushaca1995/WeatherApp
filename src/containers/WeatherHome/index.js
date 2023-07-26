8ed78ec6dc163671771e448b4d4e8fd5a1c7a1d3
Anusha Antony <52962772+Anushaca1995@users.noreply.github.com>
2023-07-26 18:36:58 +0100

    Added geo location and permissions to ios, android

--- src/containers/WeatherHome/index.js
+++ src/containers/WeatherHome/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import styles from './styles';
 import {CustomButton} from '../../components';
+import {LocHelper} from '../../helpers';
 
 const WeatherHome = ({navigation}) => {
+  const [userLoc, setUserLoc] = useState();
+  useEffect(() => {
+    LocHelper.checkLocationPermission(
+      () => {
+        LocHelper.fetchUserLocation(
+          loc => {
+            setUserLoc(loc);
+            console.log(loc);
+          },
+          error => {
+            console.log(error);
+          },
+        );
+      },
+      err => {
+        console.log(err);
+      },
+    );
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.caption}>Weather Home</Text>
---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+import LocHelper from './LocHelper';
+
+export {LocHelper};
