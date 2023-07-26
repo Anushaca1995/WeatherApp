fd4c1b220b29b9064347a0186ed83f18878cc20a
Anusha Antony <52962772+Anushaca1995@users.noreply.github.com>
2023-07-26 14:47:09 +0100

    Created basic react-native app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, useColorScheme, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={{flex: 1}}>
+      <Text style={{textAlign: 'center'}}>Weather App</Text>
+    </View>
+  );
+}
+
+export default App;
