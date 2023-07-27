c99387b3d555d105ef9e78aef736adeab28bfb38
Anusha Antony <52962772+Anushaca1995@users.noreply.github.com>
2023-07-27 12:52:23 +0100

    fetch weather api

--- src/components/CustomButton/styles.js
+++ src/components/CustomButton/styles.js
@@ -1,19 +1,18 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   button: {
     margin: 10,
-    textAlign: 'center',
-    backgroundColor: '#400080',
-    justifyContent: 'center',
-    color: 'white',
+    textAlign: "center",
+    backgroundColor: "#400080",
+    justifyContent: "center",
+    color: "white",
     padding: 10,
   },
   buttonText: {
-    color: '#cce6ff',
-    textAlign: 'center',
+    color: "#cce6ff",
+    textAlign: "center",
     fontSize: 15,
-    fontFamily: 'Poppins-Bold',
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
--- src/containers/WeatherHome/index.js
+++ src/containers/WeatherHome/index.js
@@ -1,35 +1,63 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
-import styles from './styles';
-import {CustomButton} from '../../components';
-import {LocHelper} from '../../helpers';
+import React, { useEffect, useState } from "react";
+import { Text, View, Alert, ActivityIndicator } from "react-native";
+import styles from "./styles";
+import { CustomButton } from "../../components";
+import { LocHelper } from "../../helpers";
 
-const WeatherHome = ({navigation}) => {
+const WeatherHome = ({ navigation }) => {
   const [userLoc, setUserLoc] = useState();
+  const [foreCast, setForeCast] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+  const weatherAPIKey = "6a0255bff2f1816296816573eb6f389f";
+  let weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?exclude=minutely&units=metric&appid=${weatherAPIKey}`;
+
   useEffect(() => {
     LocHelper.checkLocationPermission(
       () => {
         LocHelper.fetchUserLocation(
-          loc => {
+          (loc) => {
             setUserLoc(loc);
-            console.log(loc);
+            console.log("User Location", loc);
+            fetchForeCast();
           },
-          error => {
+          (error) => {
             console.log(error);
-          },
+            Alert.alert("Oops", "Something went wrong");
+          }
         );
       },
-      err => {
+      (err) => {
         console.log(err);
-      },
+      }
     );
   }, []);
+
+  const fetchForeCast = async () => {
+    setRefresh(true);
+    weatherUrl = `${weatherUrl}&lat=${userLoc.latitude}&lon=${userLoc.longitude}`;
+    const response = await fetch(weatherUrl);
+    console.log("url", weatherUrl);
+    const data = await response.json();
+    console.log("forecast", data);
+    const current = data.current.weather[0];
+    console.log("Current weather", current);
+    if (!response.ok) {
+      Alert.alert("Error", "Something went wrong");
+    } else {
+      setForeCast(data);
+    }
+    setIsLoading(false);
+    setRefresh(false);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.caption}>Weather Home</Text>
+      {isLoading && <ActivityIndicator size="large" color="purple" />}
       <CustomButton
-        buttonText={'Go to Forecast'}
-        handleClick={() => navigation.navigate('ForeCast', {})}
+        buttonText={"Go to Forecast"}
+        handleClick={() => navigation.navigate("ForeCast", {})}
       />
     </View>
   );
