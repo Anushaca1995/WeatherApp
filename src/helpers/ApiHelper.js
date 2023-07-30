5e9d00badc2b5dc3cb24b5db32d9ef4095a9dafe
Anusha Antony <52962772+Anushaca1995@users.noreply.github.com>
2023-07-30 16:28:40 +0100

    final fixes for code

--- src/helpers/ApiHelper.js
+++ src/helpers/ApiHelper.js
@@ -16,14 +16,6 @@ const api = create({
 });
 
 class ApiHelper {
-  async post(url, data, headers) {
-    const response = await api.post(url, data, { headers: headers });
-
-    return new Promise((resolve, reject) => {
-      this.handlePromise(resolve, reject, response);
-    });
-  }
-
   async get(url, data, headers) {
     const response = await api.get(url, data, { headers: headers });
 
@@ -31,10 +23,6 @@ class ApiHelper {
       this.handlePromise(resolve, reject, response);
     });
   }
-
-  async delete(url, data, headers) {}
-  async put(url, data, headers) {}
-  async postImage(url, data, headers) {}
 
   handlePromise = (resolve, reject, response) => {
     if (response.status == null) {
